6ccdeaa9226e1bed3f691f30d1f9feecb953a166
Constantin Sharypov <shariktlt@gmail.com>
2022-02-17 11:07:15 +0300

    Настройка jndi datasource

---
+++ backend/src/main/java/ru/edu/project/backend/app/TomcatConfig.java
@@ -0,0 +1,83 @@
+package ru.edu.project.backend.app;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.startup.Tomcat;
+import org.apache.tomcat.util.descriptor.web.ContextResource;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.boot.web.embedded.tomcat.TomcatWebServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jndi.JndiObjectFactoryBean;
+
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+@Configuration
+public class TomcatConfig {
+
+    /**
+     * Конфигурация jndi для встроенного Tomcat.
+     *
+     * @return tomcatServletWebServerFactory
+     */
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory() {
+        return new TomcatServletWebServerFactory() {
+
+            /**
+             * Активация jndi.
+             *
+             * @param tomcat
+             * @return tomcat
+             */
+            @Override
+            protected TomcatWebServer getTomcatWebServer(final Tomcat tomcat) {
+                tomcat.enableNaming();
+                return super.getTomcatWebServer(tomcat);
+            }
+
+            /**
+             * Создание ресурса datasource.
+             *
+             * @param context
+             */
+            @Override
+            protected void postProcessContext(final Context context) {
+                ContextResource resource = new ContextResource();
+                resource.setName("jdbc/backendDbLink");
+                resource.setType(DataSource.class.getName());
+
+                //Можно вынести в свойства или оставить константы как есть
+                //так как это применяется только для локального запуска
+                //на внешнем сервере развернутый war будет использовать
+                //ресурсы сервера
+                resource.setProperty("factory", "com.zaxxer.hikari.HikariJNDIFactory");
+                resource.setProperty("driverClassName", "org.h2.Driver");
+                resource.setProperty("jdbcUrl", "jdbc:h2:./backend_db");
+                resource.setProperty("username", "sa");
+                resource.setProperty("password", "");
+
+                context.getNamingResources()
+                        .addResource(resource);
+            }
+        };
+    }
+
+    /**
+     * Извлечение jndi datasource.
+     *
+     * @return datasource
+     * @throws IllegalArgumentException
+     * @throws NamingException
+     */
+    @Bean(destroyMethod = "")
+    public DataSource jndiDataSource() throws IllegalArgumentException, NamingException {
+        JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+        bean.setJndiName("java:comp/env/jdbc/backendDbLink");
+        bean.setProxyInterface(DataSource.class);
+        bean.setLookupOnStartup(false);
+        bean.afterPropertiesSet();
+        return (DataSource) bean.getObject();
+    }
+
+}
